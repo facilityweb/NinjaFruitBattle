41f7a9473088223d7fa2c55220a4a17ec26062ed
igor de andrade monteiro <igor_fat@hotmail.com>
2019-06-22 21:37:31 -0300

    classe domain passa a ser .net standard, projeto windows alterado para utilizar o vs 2019

--- NinjaBattle.Droid/Properties/AssemblyInfo.cs
+++ NinjaBattle.Droid/Properties/AssemblyInfo.cs
@@ -7,21 +7,14 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NinjaBattle.Droid")]
-[assembly: AssemblyProduct("NinjaBattle.Droid")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NinjaBattle.Droid")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3a10b926-5449-4b54-ba92-2849a56b480c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +28,3 @@ using Android.App;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-// Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
